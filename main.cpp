3d4c0c9f92e7525bf6cfaab17a00979fb14e407e
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-06 18:13:15 +0800

    提交代码

--- main.cpp
+++ main.cpp
@@ -8,9 +8,56 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <conio.h>
-#include <io.h>
+#include <unistd.h>
+// #include <conio.h>
+// #include <io.h>
 
+#include <termios.h>
+#include <stdio.h>
+
+static struct termios old, current;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo) 
+{
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  current = old; /* make new settings same as old settings */
+  current.c_lflag &= ~ICANON; /* disable buffered i/o */
+  if (echo) {
+      current.c_lflag |= ECHO; /* set echo mode */
+  } else {
+      current.c_lflag &= ~ECHO; /* set no echo mode */
+  }
+  tcsetattr(0, TCSANOW, &current); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void) 
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo) 
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+
+/* Read 1 character without echo */
+char _getch(void) 
+{
+  return getch_(0);
+}
+
+/* Read 1 character with echo */
+char getche(void) 
+{
+  return getch_(1);
+}
 
 //游戏之中可能出现的商品总数目
 #define GOODS_LIST_MAX 16
@@ -438,7 +485,7 @@ void data_mngmnt( player * pplayer ){
 	data dat = { { 0, 0, 0 } };
 
 	//检查是否存在存档文件
-	if( _access( "./save.sav", 0 ) != 0 ){
+	if( access( "./save.sav", 0 ) != 0 ){
 
 		FILE * fp = NULL;
 
